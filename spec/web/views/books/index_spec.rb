28aa56ff19494f8e62db05589b31ad6342dcc9cf
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-09-09 01:59:02 +0700

    Add unit tests for Books related features

--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -1,10 +1,38 @@
 RSpec.describe Web::Views::Books::Index, type: :view do
-  let(:exposures) { Hash[format: :html] }
-  let(:template)  { Hanami::View::Template.new('apps/web/templates/books/index.html.erb') }
-  let(:view)      { described_class.new(template, exposures) }
-  let(:rendered)  { view.render }
+  let(:exposures) { Hash[books: []] }
+  let(:template) do
+    Hanami::View::Template.new('apps/web/templates/books/index.html.haml')
+  end
+  let(:view) { described_class.new(template, exposures) }
+  let(:rendered) { view.render }
 
-  it 'exposes #format' do
-    expect(view.format).to eq exposures.fetch(:format)
+  it 'exposes #books' do
+    expect(view.books).to eq(exposures.fetch(:books))
+  end
+
+  context 'when there are no books' do
+    it 'shows a placeholder message' do
+      expect(rendered)
+        .to include('<p class="placeholder">There are no books yet.</p>')
+    end
+  end
+
+  context 'when there are books' do
+    let(:book1) { Book.new(title: 'Refactoring', author: 'Martin Fowler') }
+    let(:book2) do
+      Book.new(title: 'Domain Driven Design', author: 'Eric Evans')
+    end
+    let(:exposures) { Hash[books: [book1, book2]] }
+
+    it 'lists them all' do
+      expect(rendered.scan(/class="book"/).length).to eq(2)
+      expect(rendered).to include('Refactoring')
+      expect(rendered).to include('Domain Driven Design')
+    end
+
+    it 'hides the placeholder message' do
+      expect(rendered)
+        .to_not include('<p class="placeholder">There are no books yet.</p>')
+    end
   end
 end
